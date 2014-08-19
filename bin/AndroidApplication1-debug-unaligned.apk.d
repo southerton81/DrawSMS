f62b690898d0989d33bbb697929d7d23c0f8549a
Dmitriy Kurovsky <dkurovskiy@ukr.net>
2014-08-19 13:11:26 +0300

    deleted /bin

--- bin/AndroidApplication1-debug-unaligned.apk.d
+++
@@ -1,4 +0,0 @@
-C:\DrawSMS\AndroidApplication1\bin\AndroidApplication1-debug-unaligned.apk : \
-C:\DrawSMS\AndroidApplication1\bin\AndroidApplication1.ap_ \
-C:\DrawSMS\AndroidApplication1\bin\classes.dex \
-C:\DrawSMS\AndroidApplication1\libs\GoogleAdMobAdsSdk-6.4.1.jar \
--- bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-C:\DrawSMS\AndroidApplication1\bin\AndroidManifest.xml : \
-C:\DrawSMS\AndroidApplication1\AndroidManifest.xml \
